82dc60263428824b45d93c6793e618c7eaf8330b
aii-linebot-project <u511130@gmail.com>
2019-12-28 16:55:12 +0800

    sticker & msg replyer

--- main_app.py
+++ main_app.py
@@ -11,6 +11,7 @@ line_bot_api = LineBotApi(
 handler = WebhookHandler('0300c17210cfea52f0499e9cf80d7984')  # ?  318797af646feaa757db0b6c6e08561c
 
 
+# Homepage
 @main_app.route('/', methods=["POST", "GET"])
 def index():
 	return render_template('index.html')  # main_app.send_static_file
@@ -33,14 +34,26 @@ def default(event):
 	print(f'{event} event catched')
 
 
+# 基本複誦訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_messages(event):
-	txt = event.message.text
+	msg = event.message.text(encode='utf-8')
 	line_bot_api.reply_message(
 		event.reply_token,
-		TextSendMessage(text=txt)
+		TextSendMessage(text=msg)
 	)
-	
+
+
+# 基本回傳貼圖
+@handler.add(MessageEvent, message=StickerMessage)
+def handle_sticker_message(event):
+	p_id = event.message.package_id
+	s_id = event.message.package_id
+	line_bot_api.reply_message(
+		event.reply_token,
+		StickerSendMessage(package_id=p_id, sticker_id=s_id)
+	)
+
 
 if __name__ == "__main__":
 	main_app.run(debug=True)  # , host='127.0.0.1', port=80
