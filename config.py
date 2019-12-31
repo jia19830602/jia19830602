755950670e9d2bb2aed0e0add6c0086dc006767a
aii-linebot-project <u511130@gmail.com>
2019-12-31 12:50:12 +0800

    fix config

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ load_dotenv()
 
 
 CHANNEL_ID = getenv('CHANNEL_ID')
-assert CHANNEL_ID
+
 CHANNEL_SECRET = getenv('CHANNEL_SECRET')
-assert CHANNEL_SECRET
+
 CHANNEL_ACCESS_TOKEN = getenv('CHANNEL_ACCESS_TOKEN')
-assert CHANNEL_ACCESS_TOKEN
+
 
 
 
