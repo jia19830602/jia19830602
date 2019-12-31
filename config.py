05b122943d64246b1299431afb6d2523aa616eb8
aii-linebot-project <u511130@gmail.com>
2019-12-31 12:27:19 +0800

    remove None from config

--- config.py
+++ config.py
@@ -3,14 +3,15 @@ from os import getenv
 
 from dotenv import load_dotenv
 
-load_dotenv()
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 
-CHANNEL_ID = getenv('CHANNEL_ID', None)
+CHANNEL_ID = getenv('CHANNEL_ID')
 assert CHANNEL_ID
-CHANNEL_SECRET = getenv('CHANNEL_SECRET', None)
+CHANNEL_SECRET = getenv('CHANNEL_SECRET')
 assert CHANNEL_SECRET
-CHANNEL_ACCESS_TOKEN = getenv('CHANNEL_ACCESS_TOKEN', None)
+CHANNEL_ACCESS_TOKEN = getenv('CHANNEL_ACCESS_TOKEN')
 assert CHANNEL_ACCESS_TOKEN
 
 
