7d29054ecea75d219379b7d40adb2328a3d65da2
aii-linebot-project <u511130@gmail.com>
2020-01-01 12:49:47 +0800

    resume .env v2-1

--- main_app.py
+++ main_app.py
@@ -6,8 +6,8 @@ import config
 
 main_app = Flask(__name__, static_folder='.', static_url_path='')  #
 # main_app.debug = True
-line_bot_api = LineBotApi(config.CHANNEL_ACCESS_TOKEN)  #
-handler = WebhookHandler(config.CHANNEL_SECRET)  # ?  318797af646feaa757db0b6c6e08561c getenv('CHANNEL_SECRET')
+line_bot_api = LineBotApi('YXeGjBJc3YAcQoa2BkNl2+34VpPGczVRn76/WUGm24WV9pKB1a+ndqnF0zhLuM0Sr2lp8m1tasxeEf0XgdMLloCQCIDiR9lzPExqtbKxii/YMtPGlBp9zVUBeSbUdEwaKYUobRES+9H7P5JtNoFl6wdB04t89/1O/w1cDnyilFU=')  #
+handler = WebhookHandler('0300c17210cfea52f0499e9cf80d7984')  # ?  318797af646feaa757db0b6c6e08561c getenv('CHANNEL_SECRET')
 
 
 # Homepage
