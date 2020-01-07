e681b8c10e8b30882b404821f7ea8fceb0696534
aii-linebot-project <u511130@gmail.com>
2020-01-07 14:05:49 +0800

    fix connect

--- apps/__init__.py
+++ apps/__init__.py
@@ -10,8 +10,8 @@ main_app = Flask(__name__)  # , static_folder='.', static_url_path=''
 CORS(main_app)  # 全域跨域請求
 
 main_app.secret_key = ''
-main_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-main_app.config['SQLALCHEMY_DATABASE_URI'] = configs.SQLALCHEMY_DATABASE_URI
+# main_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# main_app.config['SQLALCHEMY_DATABASE_URI'] = configs.SQLALCHEMY_DATABASE_URI
 
 line_db = SQLAlchemy(main_app)
 # apps.debug = True
