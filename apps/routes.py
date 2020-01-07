5df431b06b7720f91874d586a4219b0b47fa82f2
aii-linebot-project <u511130@gmail.com>
2020-01-07 17:39:05 +0800

    form msg event

--- apps/routes.py
+++ apps/routes.py
@@ -34,8 +34,57 @@ def default(default_event):
 	print(f'{default_event} event catched')
 
 
-# # 基本複誦訊息&關鍵字打招呼
-# @handler.add(MessageEvent, message=TextMessage)
+# 基本複誦訊息&關鍵字打招呼
+@handler.add(MessageEvent, message=TextMessage)
+def handle_messages(mgs_event):
+	_id = mgs_event.source.user_id  # get user ID
+	profile = line_bot_api.get_profile(_id)  # get personal info
+	_name = profile.display_name  # storage user display name
+	user_msg = mgs_event.message.text  # read text which user passed in
+
+	greet_list = ['你好', '嗨', '哈囉', 'hi', 'hey']
+
+	if user_msg in greet_list:
+		greet_user = f'Hello! {_name} '
+		reply = greet_user
+
+	else:
+		reply = user_msg
+
+	reply_msg = TextSendMessage(text=reply)
+	line_bot_api.reply_message(
+			mgs_event.reply_token,
+			reply_msg
+	)
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def find_news(mgs_event):
+	user_msg = mgs_event.message.text
+
+	def tech_news():
+		target_url = 'https://technews.tw/'
+		print('Start parsing movie ...')
+		rs = requests.session()
+		res = rs.get(target_url, verify=False)
+		res.encoding = 'utf-8'
+		soup = BeautifulSoup(res.text, 'html.parser')
+		_content = ""
+
+		for _index, data in enumerate(soup.select('article div h1.entry-title a')):
+			if _index == 12:
+				return _content
+			title = data.text
+			link = data['href']
+			_content += '{}\n{}\n\n'.format(title, link)
+		return _content
+
+	if user_msg == "新聞":
+		content = tech_news()
+		line_bot_api.reply_message(
+				mgs_event.reply_token,
+				TextSendMessage(text=content)
+		)
 
 
 # 基本回傳貼圖
@@ -46,58 +95,6 @@ def handle_sticker_message(sticker_event):
 	line_bot_api.reply_message(
 			sticker_event.reply_token,
 			StickerSendMessage(package_id=p_id, sticker_id=s_id)
-	)
-
-
-@handler.add(MessageEvent, message=TextMessage)
-# def find_news(mgs_event):
-#
-# 	def tech_news():
-# 		target_url = 'https://technews.tw/'
-# 		print('Start parsing movie ...')
-# 		rs = requests.session()
-# 		res = rs.get(target_url, verify=False)
-# 		res.encoding = 'utf-8'
-# 		soup = BeautifulSoup(res.text, 'html.parser')
-# 		_content = ""
-#
-# 		for _index, data in enumerate(soup.select('article div h1.entry-title a')):
-# 			if _index == 12:
-# 				return _content
-# 			title = data.text
-# 			link = data['href']
-# 			_content += '{}\n{}\n\n'.format(title, link)
-# 		return _content
-#
-# 	user_msg = mgs_event.message.text
-#
-# 	if user_msg == "新聞":
-# 		content = tech_news()
-# 		line_bot_api.reply_message(
-# 				mgs_event.reply_token,
-# 				TextSendMessage(text=content)
-# 		)
-
-
-def handle_messages(mgs_event2):
-	_id = mgs_event2.source.user_id  # get user ID
-	profile = line_bot_api.get_profile(_id)  # get personal info
-	_name = profile.display_name  # storage user display name
-	user_msg2 = mgs_event2.message.text  # read text which user passed in
-
-	greet_list = ['你好', '嗨', '哈囉', 'hi', 'hey']
-
-	if user_msg2 in greet_list:
-		greet_user = f'Hello! {_name} '
-		reply = greet_user
-
-	else:
-		reply = user_msg2
-
-	reply_msg = TextSendMessage(text=reply)
-	line_bot_api.reply_message(
-			mgs_event2.reply_token,
-			reply_msg
 	)
 
 
