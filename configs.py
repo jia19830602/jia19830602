5f88154fc3716f0c844ae1e61d03d63d3bd6ff82
aii-linebot-project <u511130@gmail.com>
2020-01-07 16:20:21 +0800

    connect fix v3

--- configs.py
+++ configs.py
@@ -1,13 +1,13 @@
 from os import getenv
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
+# env_path = Path('.') / '.env'
+load_dotenv(find_dotenv())  # dotenv_path=env_path
 
 CHANNEL_ID = getenv('CHANNEL_ID')
 CHANNEL_SECRET = getenv('CHANNEL_SECRET')
 CHANNEL_ACCESS_TOKEN = getenv('CHANNEL_ACCESS_TOKEN')
 
 SQLALCHEMY_DATABASE_URI = getenv('CLEARDB_DATABASE_URL')
-print(CHANNEL_ACCESS_TOKEN)
+
